7e6c29e25637396fab173b0cc6fb25288b3bd0ea
rafik.mnatsakanyan <rafik.mnatsakanyan@polixis.com>
2025-01-29 12:16:44 +0400

    crud api with mongo

--- src/main/java/com/gRPC/gRPC_Java_Service/blog/client/BlogClient.java
+++ src/main/java/com/gRPC/gRPC_Java_Service/blog/client/BlogClient.java
@@ -1,5 +1,6 @@
 package com.gRPC.gRPC_Java_Service.blog.client;
 
+import com.google.protobuf.Empty;
 import com.proto.blog.Blog;
 import com.proto.blog.BlogId;
 import com.proto.blog.BlogServiceGrpc;
@@ -16,6 +17,7 @@ public class BlogClient {
         .build();
 
 
+    run(channel);
 
     System.out.println("Shutting Down For Client ...");
     channel.shutdown();
@@ -26,8 +28,54 @@ public class BlogClient {
     BlogServiceGrpc.BlogServiceBlockingStub stub = BlogServiceGrpc.newBlockingStub(channel);
     BlogId blogId=createBlog(stub);
 
-    if (blogId==null){
+    if (blogId==null)
       return;
+
+    readBlog(stub,blogId);
+    updateBlog(stub,blogId);
+    listBlogs(stub);
+    deleteBlog(stub,blogId);
+  }
+
+  private static void readBlog(BlogServiceGrpc.BlogServiceBlockingStub stub,BlogId blogId){
+    try {
+      Blog readResponse=stub.readBlog(blogId);
+      System.out.println("Blog read : "+readResponse);
+    }catch (StatusRuntimeException e){
+      System.out.println("Couldn't read blog");
+      e.printStackTrace();
+    }
+  }
+
+  private static void updateBlog(BlogServiceGrpc.BlogServiceBlockingStub stub,BlogId blogId){
+    try {
+      Blog newBlog=Blog.newBuilder()
+          .setId(blogId.getId())
+          .setAuthor("Jack")
+          .setTitle("New Blog (Changed)")
+          .setContent("Hello world this is my first blog! I've added some content")
+          .build();
+
+      stub.updateBlog(newBlog);
+      System.out.println("Blog updated : "+newBlog);
+    }catch (StatusRuntimeException e){
+      System.out.println("Couldn't update blog");
+      e.printStackTrace();
+    }
+  }
+
+  private static void listBlogs(BlogServiceGrpc.BlogServiceBlockingStub stub){
+    stub.listBlogs(Empty.getDefaultInstance()).forEachRemaining(System.out::println);
+  }
+
+
+  private static void deleteBlog(BlogServiceGrpc.BlogServiceBlockingStub stub,BlogId blogId){
+    try {
+      stub.deleteBlog(blogId);
+      System.out.println("Blog deleted : "+blogId);
+    }catch (StatusRuntimeException e){
+      System.out.println("Couldn't delete blog");
+      e.printStackTrace();
     }
   }
 
--- src/main/java/com/gRPC/gRPC_Java_Service/blog/server/BlogServerImpl.java
+++ src/main/java/com/gRPC/gRPC_Java_Service/blog/server/BlogServerImpl.java
@@ -1,9 +1,15 @@
 package com.gRPC.gRPC_Java_Service.blog.server;
 
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.set;
+
+import com.google.protobuf.Empty;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.InsertOneResult;
 import com.proto.blog.Blog;
 import com.proto.blog.BlogId;
@@ -11,6 +17,7 @@ import com.proto.blog.BlogServiceGrpc;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class BlogServerImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
@@ -50,5 +57,115 @@ public class BlogServerImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
     responseObserver.onNext(BlogId.newBuilder().setId(id).build());
     responseObserver.onCompleted();
+  }
+
+
+  @Override
+  public void readBlog(BlogId request, StreamObserver<Blog> responseObserver) {
+    if (request.getId().isEmpty()){
+      responseObserver.onError(Status.INVALID_ARGUMENT
+                                   .withDescription("Blog id cannot be empty")
+                                   .asRuntimeException());
+      return;
+    }
+
+    String id=request.getId();
+
+    Document result=mongoCollection.find(eq("_id",new ObjectId(id))).first();
+
+    if (result==null){
+      responseObserver.onError(Status.NOT_FOUND
+                                   .withDescription("Blog wasnot found")
+                                   .augmentDescription("BlogId "+id)
+                                   .asRuntimeException()
+      );
+      return;
+    }
+    responseObserver.onNext(Blog.newBuilder().setAuthor(result.getString("author"))
+                                .setTitle(result.getString("title"))
+                                .setContent(result.getString("content"))
+                                .build());
+    responseObserver.onCompleted();
+  }
+
+
+  @Override
+  public void updateBlog(Blog request, StreamObserver<Empty>responseObserver) {
+    if (request.getId().isEmpty()){
+      responseObserver.onError(Status.INVALID_ARGUMENT
+                                   .withDescription("Blog id cannot be empty")
+                                   .asRuntimeException());
+      return;
+    }
+
+    String id=request.getId();
+    Document result=mongoCollection.findOneAndUpdate(eq("_id",new ObjectId(id)),
+                                                     combine(set("author",request.getAuthor()),
+                                                             set("title",request.getTitle()),
+                                                             set("content",request.getContent())
+                                                             ));
+
+    if (result==null){
+      responseObserver.onError(Status.NOT_FOUND
+      .withDescription("Blog was not found")
+                                   .augmentDescription("BlogId "+id).asRuntimeException());
+      return;
+    }
+    responseObserver.onNext(Empty.getDefaultInstance());
+    responseObserver.onCompleted();
+  }
+
+
+  @Override
+  public void listBlogs(Empty request,StreamObserver<Blog>responseObserver){
+    for (Document document:mongoCollection.find()){
+      responseObserver.onNext(Blog.newBuilder()
+                                  .setId(document.getObjectId("_id").toString())
+                                  .setAuthor(document.getString("author"))
+                                  .setTitle(document.getString("title"))
+                                  .setContent(document.getString("content"))
+                                  .build());
+    }
+
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void deleteBlog(BlogId  request,StreamObserver<Empty>responseObserver){
+    if (request.getId().isEmpty()){
+      responseObserver.onError(Status.INVALID_ARGUMENT
+                                   .withDescription("Blog id cannot be empty")
+                                   .asRuntimeException());
+      return;
+    }
+
+    String id=request.getId();
+    DeleteResult result;
+
+    try {
+      result = mongoCollection.deleteOne(eq("_id",new ObjectId(id)));
+    }catch (MongoException e){
+      responseObserver.onError(Status.INTERNAL
+                                   .withDescription("The blog couldn't be deleted ")
+      .asRuntimeException());
+      return;
+    }
+      if (!result.wasAcknowledged()){
+        responseObserver.onError(Status.INTERNAL
+                                     .withDescription("The blog couldn't be deleted ")
+                                     .asRuntimeException());
+        return;
+      }
+
+      if (result.getDeletedCount()==0){
+        responseObserver.onError(Status.NOT_FOUND
+        .withDescription("Blog was not found")
+                                     .augmentDescription("BlogId "+id)
+                                     .asRuntimeException());
+        return;
+      }
+
+      responseObserver.onNext(Empty.getDefaultInstance());
+      responseObserver.onCompleted();
   }
 }
